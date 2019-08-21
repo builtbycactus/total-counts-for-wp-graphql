65740430835c5a9b97e458304e4276883b004737
Daryll Doyle <daryll@enshrined.co.uk>
2019-08-21 12:53:34 +0100

    Add Plugin and readme

---
+++ README.md
@@ -0,0 +1,67 @@
+# Total Counts for WP-GraphQL
+
+As it stands WP-GraphQL doesn't implement the `totalCount` field. This is due to a number of reasons, including the performance impact that would come with such a query.
+
+We found that we needed this for a site being developed, so built up this basic plugin.
+
+Whilst this doesn't implement the `totalCount` field as per the [GraphQL docs](https://graphql.org/learn/pagination/#end-of-list-counts-and-connections), it does add a `total` field to the `pageInfo` object that is used for pagination. This seemed like the most sensible place to put it, as 99% of the time, we'll be using this field to pull back the total number of items, alongside our paginated result.
+
+
+
+## Retrieving the total count.
+
+To retrieve to total count, it's a simple case of requesting the `total` field with your `pageInfo`. See below:
+
+```json
+{
+  posts(first:1) {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+    pageInfo {
+      total
+      hasNextPage
+      endCursor
+    }
+  }
+}
+```
+
+This will then give you a result as such:
+
+```json
+{
+  "data": {
+    "posts": {
+      "edges": [
+        {
+          "node": {
+            "id": "cG9zdDozOA==",
+            "title": "Post B"
+          }
+        }
+      ],
+      "pageInfo": {
+        "total": 2,
+        "hasNextPage": true,
+        "endCursor": "YXJyYXljb25uZWN0aW9uOjM4"
+      }
+    }
+  }
+}
+```
+
+
+
+As you can see, the total number of items is accessible via `data.posts.pageInfo.total`.
+
+
+
+## Contributions
+
+Contributions are welcome. This was a very quick build and as such there are probably performance gains to be had.
+
+Feel free to make a PR against this repo!
---
+++ total-counts-for-wp-graphql.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Plugin Name:     Total Counts for WPGraphQL
+ * Plugin URI:      https://github.com/builtbycactus/total-counts-for-wp-graphql
+ * Description:     Adds Total Count to the WPGraphQL Schema
+ * Author:          Built By Cactus
+ * Author URI:      https://builtbycactus.co.uk/
+ * Text Domain:     cactus-gqltc
+ * Domain Path:     /languages
+ * Version:         0.0.1
+ */
+
+namespace Cactus\GQLTC;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Initialize the plugin
+ */
+function init() {
+	/**
+	 * If WPGraphQL is not active, show the admin notice
+	 */
+	if ( ! class_exists( 'WPGraphQL' ) ) {
+		add_action( 'admin_init', __NAMESPACE__ . '\show_admin_notice' );
+	} else {
+		add_filter( 'graphql_connection_page_info', __NAMESPACE__ . '\resolve_total_field', 10, 2 );
+		add_filter( 'graphql_post_object_connection_query_args', __NAMESPACE__ . '\count_total_rows' );
+		add_filter( 'graphql_register_types', __NAMESPACE__ . '\register_total_field' );
+	}
+}
+
+add_action( 'init', __NAMESPACE__ . '\init' );
+
+/**
+ * Show admin notice to admins if this plugin is active but either ACF and/or WPGraphQL
+ * are not active
+ *
+ * @return bool
+ */
+function show_admin_notice() {
+	/**
+	 * For users with lower capabilities, don't show the notice
+	 */
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return false;
+	}
+
+	add_action(
+		'admin_notices',
+		function () {
+			?>
+            <div class="error notice">
+                <p><?php _e( 'WPGraphQL must be active for "total-counts-for-wpgraphql" to work', 'cactus-gqltc' ); ?></p>
+            </div>
+			<?php
+		}
+	);
+
+	return true;
+}
+
+/**
+ * Resolve the total field.
+ *
+ * @param $page_info
+ * @param $connection
+ *
+ * @return mixed
+ */
+function resolve_total_field( $page_info, $connection ) {
+	$page_info['total'] = null;
+	if ( $connection->get_query() instanceof \WP_Query ) {
+		if ( isset( $connection->get_query()->found_posts ) ) {
+			$page_info['total'] = (int) $connection->get_query()->found_posts;
+		}
+	}
+
+	return $page_info;
+}
+
+/**
+ * Tell the underlying WP_Query to count the total number of rows.
+ *
+ * @param $args
+ *
+ * @return mixed
+ */
+function count_total_rows( $args ) {
+	$args['no_found_rows'] = false;
+
+	return $args;
+}
+
+/**
+ * Register a total field for queries.
+ */
+function register_total_field() {
+	register_graphql_field( 'WPPageInfo', 'total', [
+		'type' => 'Int',
+	] );
+}
+
+;
